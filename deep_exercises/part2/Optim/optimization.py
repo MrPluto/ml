f042bb13939c4473019332ce7468310fa1d1d3ae
MrPluto <457808167@qq.com>
2018-01-09 15:28:29 +0800

    optimization method conclusion

--- deep_exercises/part2/Optim/optimization.py
+++ deep_exercises/part2/Optim/optimization.py
@@ -17,7 +17,6 @@ from opt_utils import load_params_and_grads, initialize_parameters, forward_prop
 from opt_utils import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
 from testCases import *
 
-%matplotlib inline
 plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
@@ -26,7 +25,7 @@ def update_parameters_with_gd(parameters,grads,learning_rate):
     '''
     '''
 
-    layers = len(parameters) / 2 # one couple of w and b respect one layer
+    layers = len(parameters) // 2 # one couple of w and b respect one layer
 
     for layer in range(layers):
         parameters['W' + str( layer + 1 )] = parameters['W' + str( layer + 1 )] - learning_rate * grads['dW' + str( layer + 1 )]
@@ -35,38 +34,185 @@ def update_parameters_with_gd(parameters,grads,learning_rate):
 
     return parameters
 
-# Batch Gradient Descent
-def gd():
-    X = data_input
-Y = labels
-parameters = initialize_parameters(layers_dims)
-for i in range(0, num_iterations):
-    # Forward propagation
-    a, caches = forward_propagation(X, parameters)
-    # Compute cost.
-    cost = compute_cost(a, Y)
-    # Backward propagation.
-    grads = backward_propagation(a, caches, parameters)
-    # Update parameters.
-    parameters = update_parameters(parameters, grads)
-    pass
+def random_mini_batches(X,Y,mini_batch_size=64,seed=0):
 
-# Stochastic gradient Descent
-def sgd():
-    X = data_input
-    Y = labels
-    parameters = initialize_parameters(layers_dims)
-    m = X.shape[1] # number of examples
+    np.random.seed(seed)
+    m = X.shape[1]
+    mini_batches = []
 
-    for i in range(0, num_iterations):
-        for j in range(0,m):
-        # Forward propagation
-        a, caches = forward_propagation(X[:,j], parameters)
-        # Compute cost.
-        cost = compute_cost(a, Y[:,j])
-        # Backward propagation.
-        grads = backward_propagation(a, caches, parameters)
-        # Update parameters.
-        parameters = update_parameters(parameters, grads
+    permutation = list(np.random.permutation(m))
+
+    shuffled_X = X[:,permutation]
+    shuffled_Y = Y[:,permutation].reshape((1,m)) #keep the shape
+
+    assert(shuffled_Y.shape == (1,m))
+
+    num_complete_minibatches = int(math.floor( m / mini_batch_size ))
+
+    for i in range(0,num_complete_minibatches):
+        mini_batch_X = shuffled_X[:,i * mini_batch_size : ( i + 1 ) * mini_batch_size]
+        mini_batch_Y = shuffled_Y[:,i * mini_batch_size : ( i + 1 ) * mini_batch_size]
+
+        mini_batch = (mini_batch_X,mini_batch_Y)
+        mini_batches.append(mini_batch)
         pass
-    pass
+    if m % mini_batch_size != 0:
+        last_batch_X = shuffled_X[:,num_complete_minibatches * mini_batch_size : ]
+        last_batch_Y = shuffled_Y[:,num_complete_minibatches * mini_batch_size : ]
+        last_batch = (last_batch_X,last_batch_Y)
+
+        mini_batches.append(last_batch)
+        pass
+    return mini_batches
+
+
+def initialize_velocity(parameters):
+    length = len(parameters) // 2
+    v = {}
+
+    for l in range(length):
+        v["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l+1)])
+        v["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l+1)])
+        pass
+    return v
+
+def update_parameters_with_momentum(parameters,grads,v,learning_rate,beta=0.9):
+    length = len(parameters) // 2
+
+    for l in range(length):
+        v["dW" + str(l + 1)] = beta * v["dW" + str(l + 1)] + (1 - beta) * grads['dW' + str(l + 1)]
+        v["db" + str(l + 1)] = beta * v["db" + str(l + 1)] + (1 - beta) * grads['db' + str(l + 1)]
+        # update parameters
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * v["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * v["db" + str(l + 1)]
+
+        pass
+
+    return parameters,v
+
+
+def initialize_adam(parameters):
+
+    L = len(parameters) // 2 # number of layers in the neural networks
+    v = {}
+    s = {}
+
+    # Initialize v, s. Input: "parameters". Outputs: "v, s".
+    for l in range(L):
+    ### START CODE HERE ### (approx. 4 lines)
+        v["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        v["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l + 1)])
+
+        s["dW" + str(l+1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        s["db" + str(l+1)] = np.zeros_like(parameters["b" + str(l + 1)])
+    ### END CODE HERE ###
+
+    return v, s
+
+
+def update_parameters_with_adam(parameters,grads,s,v,t,learning_rate=0.01,beta1=0.9,beta2=0.999,epsilion=1e-8):
+
+    L = len(parameters) // 2                 # number of layers in the neural networks
+    v_corrected = {}                         # Initializing first moment estimate, python dictionary
+    s_corrected = {}                         # Initializing second moment estimate, python dictionary
+
+    # Perform Adam update on all parameters
+    for l in range(L):
+        # Moving average of the gradients. Inputs: "v, grads, beta1". Output: "v".
+        ### START CODE HERE ### (approx. 2 lines)
+        v["dW" + str(l + 1)] = beta1 * v["dW" + str(l + 1)] + (1 - beta1) * grads['dW' + str(l + 1)]
+        v["db" + str(l + 1)] = beta1 * v["db" + str(l + 1)] + (1 - beta1) * grads['db' + str(l + 1)]
+        ### END CODE HERE ###
+
+        # Compute bias-corrected first moment estimate. Inputs: "v, beta1, t". Output: "v_corrected".
+        ### START CODE HERE ### (approx. 2 lines)
+        v_corrected["dW" + str(l + 1)] = v["dW" + str(l + 1)] / (1 - np.power(beta1, t))
+        v_corrected["db" + str(l + 1)] = v["db" + str(l + 1)] / (1 - np.power(beta1, t))
+        ### END CODE HERE ###
+
+        # Moving average of the squared gradients. Inputs: "s, grads, beta2". Output: "s".
+        ### START CODE HERE ### (approx. 2 lines)
+        s["dW" + str(l + 1)] = beta2 * s["dW" + str(l + 1)] + (1 - beta2) * np.power(grads['dW' + str(l + 1)], 2)
+        s["db" + str(l + 1)] = beta2 * s["db" + str(l + 1)] + (1 - beta2) * np.power(grads['db' + str(l + 1)], 2)
+        ### END CODE HERE ###
+
+        # Compute bias-corrected second raw moment estimate. Inputs: "s, beta2, t". Output: "s_corrected".
+        ### START CODE HERE ### (approx. 2 lines)
+        s_corrected["dW" + str(l + 1)] = s["dW" + str(l + 1)] / (1 - np.power(beta2, t))
+        s_corrected["db" + str(l + 1)] = s["db" + str(l + 1)] / (1 - np.power(beta2, t))
+        ### END CODE HERE ###
+
+        # Update parameters. Inputs: "parameters, learning_rate, v_corrected, s_corrected, epsilon". Output: "parameters".
+        ### START CODE HERE ### (approx. 2 lines)
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * v_corrected["dW" + str(l + 1)] / np.sqrt(s["dW" + str(l + 1)] + epsilon)
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * v_corrected["db" + str(l + 1)] / np.sqrt(s["db" + str(l + 1)] + epsilon)
+        pass
+
+    return parameters,v,s
+
+
+
+def model(X, Y, layers_dims, optimizer, learning_rate=0.0007, mini_batch_size=64, beta=0.9,
+          beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=10000, print_cost=True):
+
+    L = len(layers_dims)             # number of layers in the neural networks
+    costs = []                       # to keep track of the cost
+    t = 0                            # initializing the counter required for Adam update
+    seed = 10                        # For grading purposes, so that your "random" minibatches are the same as ours
+
+    # Initialize parameters
+    parameters = initialize_parameters(layers_dims)
+
+    # Initialize the optimizer
+    if optimizer == "gd":
+        pass # no initialization required for gradient descent
+    elif optimizer == "momentum":
+        v = initialize_velocity(parameters)
+    elif optimizer == "adam":
+        v, s = initialize_adam(parameters)
+
+    # Optimization loop
+    for i in range(num_epochs):
+
+        # Define the random minibatches. We increment the seed to reshuffle differently the dataset after each epoch
+        seed = seed + 1
+        minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
+
+        for minibatch in minibatches:
+
+            # Select a minibatch
+            (minibatch_X, minibatch_Y) = minibatch
+
+            # Forward propagation
+            a3, caches = forward_propagation(minibatch_X, parameters)
+
+            # Compute cost
+            cost = compute_cost(a3, minibatch_Y)
+
+            # Backward propagation
+            grads = backward_propagation(minibatch_X, minibatch_Y, caches)
+
+            # Update parameters
+            if optimizer == "gd":
+                parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+            elif optimizer == "momentum":
+                parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
+            elif optimizer == "adam":
+                t = t + 1 # Adam counter
+                parameters, v, s = update_parameters_with_adam(parameters, grads, v, s,
+                                                               t, learning_rate, beta1, beta2,  epsilon)
+
+        # Print the cost every 1000 epoch
+        if print_cost and i % 1000 == 0:
+            print("Cost after epoch %i: %f" % (i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+    # plot the cost
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('epochs (per 100)')
+    plt.title("Learning rate = " + str(learning_rate))
+    plt.show()
+
+    return parameters
--- deep_exercises/part2/Optim/readme.md
+++ deep_exercises/part2/Optim/readme.md
@@ -3,23 +3,87 @@
 - You have to tune a learning rate hyperparameter **a(阿尔法)**
 - With a well-turned mini-batch size, usually it outperforms either Gradient Descent or stochastic Gradient Descent (particularly when the training set is large).
 
-####batch Gradient Descent
-
-####mini-batch Gradient Descent
+### Gradient Descent
+#### mini-batch Gradient Descent
 if mini-batch size = m, => batch Gradient Descent
 if mini-batch size = 1, => stochastic Gradient Descent
 if mini-batch size between (1,m), =>
 
-- not need to wait the epoch finished
+##### notes
+- Shuffling and Partitioning are the two steps required to build mini_batches
+- Powers of two are often chosen to be the mini-batch size
+
+##### features
+<!-- - not need to wait one epoch finished -->
 - fast
 
-####SGD (stochastic Gradient Descent)
+#### SGD (stochastic Gradient Descent)
+##### 3 for-loops in total
+- Over the number of iterations
+- Over the m training examples
+- Over the layers (update parameters)
+
+##### features
 - never converge, always kind of oscillate and wander around the region of the minimum
 - lose all your speed up from vertorization ( matrix calculate )
 
-####
+### Optimization method
+#### Momentum
 
-####Conclusion
-- if training set size <=2000, batch Gradient Descent
-- typical mini-batch size, 2^6,2^7,2^8,2^9
+##### Formula
+V(dW)t = beta1 * V(dW)t + (1 - beta1) * dW
+
+W = W - learning_rate * V(dW)t
+
+V(db)t = beta1 * V(db)t + (1 - beta1) * db
+
+b = b - learning_rate * V(db)t
+
+##### notes
+- The velocity is initialized with zeros. So the algorithm will take a few iterations to "build up" velocity and start to take bigger steps.
+- `beta` = `0`, means normal Graident Descent
+- the large beta, the smoother the update. (about 1/(1-beta), see Exponentially Weighted Averages)
+
+
+#### RMSprop
+##### Formula
+S(dW)t = beta2 * S(dW)t + (1 - beta2) * dW^2
+
+W = W - learning_rate * dW / sqrt(S(dW)t + e)
+
+S(db)t = beta2 * S(db)t + (1 - beta2) * db^2
+
+b = b - learning_rate * db / sqrt(S(db)t + e)
+
+e ≈ 1e-8  (suggested)
+
+#### Adam
+
+##### Formula
+V(dW)t = beta1 * V(dW)t + (1 - beta1) * dW
+
+V(dW)t_corrected = V(dW)t / (1 - beta1 ^ t)
+
+V(db)t = beta1 * V(db)t + (1 - beta1) * db
+
+V(db)t_corrected = V(db)t / (1 - beta1 ^ t)
+
+S(dW)t = beta2 * S(dW)t + (1 - beta2) * dW^2
+
+S(db)t = beta2 * S(db)t + (1 - beta2) * db^2
+
+W = W - learning_rate * V(dW)t_corrected / sqrt(S(dW)t + e)
+
+b = b - learning_rate * V(db)t_corrected / sqrt(S(db)t + e)
+
+beta1 -> 0.9
+
+beta2 -> 0.999
+
+e ≈ 10 ^ (-8)  (avoid dividint by zero)
+
+#### Conclusion
 - make sure mini-batch fit the CPU/GPU memory
+- The difference between gradient descent, mini-batch gradient descent and stochastic gradient descent is the number of examples you use to perform one update step.
+- You have to tune a learning rate hyperparameter  αα .
+- With a well-turned mini-batch size, usually it outperforms either gradient descent or stochastic gradient descent (particularly when the training set is large).
